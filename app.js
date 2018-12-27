28fadce6fabea84f7eb01e93c955e0f5238d8e87
Sergey Onufrienko <sergey@onufrienko.com>
2018-12-27 10:59:02 +0200

    Run prettier

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-import fs from "fs";
-import Fastify from "fastify";
-import routes from "./routes";
+import fs from 'fs';
+import Fastify from 'fastify';
+import routes from './routes';
 
 const fastify = Fastify({
-  http2: true,
-  https: {
-    allowHTTP1: true,
-    key: fs.readFileSync("./scripts/ssl/localhost.key"),
-    cert: fs.readFileSync("./scripts/ssl/localhost.crt")
-  },
-  logger: {
-    level: "error"
-  }
+	http2: true,
+	https: {
+		allowHTTP1: true,
+		key: fs.readFileSync('./scripts/ssl/localhost.key'),
+		cert: fs.readFileSync('./scripts/ssl/localhost.crt')
+	},
+	logger: {
+		level: 'error'
+	}
 });
 
 fastify.register(routes);
 
 const PORT = 4000;
 fastify.listen(PORT, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`Server listening on ${address}`);
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+	fastify.log.info(`Server listening on ${address}`);
 });
