68c86220028aa877923c8464a28fd756088b3a80
Sergey Onufrienko <sergey@onufrienko.com>
2018-12-27 11:57:19 +0200

    Run prettier

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-import usersRoutes from "./users";
+import usersRoutes from './users';
 
 export default (fastify, opts, next) => {
-  usersRoutes.forEach(route => fastify.route(route));
-  next();
+	usersRoutes.forEach(route => fastify.route(route));
+	next();
 };
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,21 +1,21 @@
-import * as usersService from "../../services/users";
+import * as usersService from '../../services/users';
 
 const routes = [
-  {
-    method: "GET",
-    url: "/users",
-    handler: usersService.getUsers
-  },
-  {
-    method: "GET",
-    url: "/users/:userId",
-    handler: usersService.getSingleUser
-  },
-  {
-    method: "POST",
-    url: "/users",
-    handler: usersService.addUser
-  }
+	{
+		method: 'GET',
+		url: '/users',
+		handler: usersService.getUsers
+	},
+	{
+		method: 'GET',
+		url: '/users/:userId',
+		handler: usersService.getSingleUser
+	},
+	{
+		method: 'POST',
+		url: '/users',
+		handler: usersService.addUser
+	}
 ];
 
 export default routes;
