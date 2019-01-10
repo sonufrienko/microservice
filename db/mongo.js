489b3779845b2a73f96cafbe11874f0b43561647
Sergey Onufrienko <sergey@onufrienko.com>
2019-01-10 16:16:40 +0200

    Add MongoDB

---
+++ db/mongo.js
@@ -0,0 +1,58 @@
+import url from 'url';
+import { MongoClient } from 'mongodb';
+import logger from '../helper/logger';
+
+export { ObjectID } from 'mongodb';
+export let db = null;
+
+const getConnectionUrl = () => {
+	const dbCred =
+		process.env.MONGO_USER.length > 0 || process.env.MONGO_PASS.length > 0
+			? `${process.env.MONGO_USER}:${process.env.MONGO_PASS}@`
+			: '';
+	const dbUrl =
+		process.env.MONGO_URL ||
+		`mongodb://${dbCred}${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${
+			process.env.MONGO_DB
+		}`;
+	return dbUrl;
+};
+
+const RECONNECT_INTERVAL = 1000;
+const CONNECT_OPTIONS = {
+	reconnectTries: 3600,
+	reconnectInterval: RECONNECT_INTERVAL,
+	useNewUrlParser: true
+};
+
+const connectWithRetry = () => {
+	const mongodbConnection = getConnectionUrl();
+	const mongoPathName = url.parse(mongodbConnection).pathname;
+	const dbName = mongoPathName.substring(mongoPathName.lastIndexOf('/') + 1);
+
+	MongoClient.connect(
+		mongodbConnection,
+		CONNECT_OPTIONS,
+		(err, client) => {
+			if (err) {
+				logger.error(
+					`MongoDB connection was failed: ${err.message}`,
+					err.message
+				);
+				setTimeout(connectWithRetry, RECONNECT_INTERVAL);
+			} else {
+				db = client.db(dbName);
+				db.on('close', () => {
+					logger.info('MongoDB connection was closed');
+					connectWithRetry();
+				});
+				db.on('reconnect', () => {
+					logger.warn('MongoDB reconnected');
+				});
+				logger.info(`MongoDB connected successfully. Database: ${dbName}.`);
+			}
+		}
+	);
+};
+
+connectWithRetry();
