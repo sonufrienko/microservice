442545a9315edf585e24857dc7502f70bbe79d84
Sergey Onufrienko <sergey@onufrienko.com>
2019-03-22 11:31:22 +0200

    Use replication on production

--- config/sequelize.js
+++ config/sequelize.js
@@ -8,9 +8,8 @@ module.exports = {
 		host: process.env.SQL_HOST,
 		port: process.env.SQL_PORT,
 		dialect: process.env.SQL_DIALECT,
-		operatorsAliases: false,
 		pool: {
-			max: 5,
+			max: Number(process.env.SQL_POOL_LIMIT),
 			min: 0,
 			acquire: 30000,
 			idle: 10000
@@ -24,13 +23,15 @@ module.exports = {
 		username: process.env.SQL_USER,
 		password: process.env.SQL_PASS,
 		database: process.env.SQL_DB,
-		host: process.env.SQL_HOST,
 		port: process.env.SQL_PORT,
 		dialect: process.env.SQL_DIALECT,
-		operatorsAliases: false,
 		logging: false,
+		replication: {
+			read: [{ host: process.env.SQL_HOST_READ }],
+			write: { host: process.env.SQL_HOST_WRITE }
+		},
 		pool: {
-			max: 100,
+			max: Number(process.env.SQL_POOL_LIMIT),
 			min: 0,
 			acquire: 30000,
 			idle: 10000
