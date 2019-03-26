a51f0e278835758bab8b9d75d6620c5852a38a1c
Sergey Onufrienko <sergey@onufrienko.com>
2019-03-26 22:00:55 +0200

    Add request validation tests

--- routes/controllers/users.js
+++ routes/controllers/users.js
@@ -1,3 +1,4 @@
+const { celebrate, Joi } = require('celebrate');
 const usersService = require('../../services/users');
 
 const getUsers = async ({ params }, res) => {
@@ -7,13 +8,29 @@ const getUsers = async ({ params }, res) => {
 
 const getSingleUser = async ({ body, params: { userId } }, res) => {
 	const user = await usersService.getSingleUser(userId);
-	res.status(user ? 200 : 404).send(user);
+	res.status(200).send(user);
 };
+
+const getSingleUserValidation = celebrate({
+	params: {
+		userId: Joi.string()
+			.regex(/^[0-9a-fA-F]{24}$/)
+			.required()
+	}
+});
 
 const addUser = async ({ body }, res) => {
 	const user = await usersService.addUser(body);
 	res.status(200).send(user);
 };
+
+const addUserValidation = celebrate({
+	body: {
+		email: Joi.string()
+			.email()
+			.required()
+	}
+});
 
 const updateUser = async ({ body, params: { userId } }, res) => {
 	await usersService.updateUser(userId, body);
@@ -28,7 +45,9 @@ const deleteUser = async ({ body, params: { userId } }, res) => {
 module.exports = {
 	getUsers,
 	getSingleUser,
+	getSingleUserValidation,
 	addUser,
+	addUserValidation,
 	updateUser,
 	deleteUser
 };
--- test/routes/users.test.js
+++ test/routes/users.test.js
@@ -17,3 +17,20 @@ test('GET /users', async () => {
 
 	expect(Array.isArray(response.body)).toBeTruthy();
 });
+
+test('GET /users/123 - validate userID', async () => {
+	const token = await security.getSignedToken(tokenPayload);
+	await request(app)
+		.get('/v1/users/123')
+		.set('Authorization', `Bearer ${token}`)
+		.expect(400)
+		.expect('Content-Type', /json/);
+});
+
+test('GET /users/53fbf4615c3b9f41c381b6a3 - validate userID', async () => {
+	const token = await security.getSignedToken(tokenPayload);
+	await request(app)
+		.get('/v1/users/53fbf4615c3b9f41c381b6a3')
+		.set('Authorization', `Bearer ${token}`)
+		.expect(200);
+});
