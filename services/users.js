8bc52b7d839fb289254377d9b75eef0baa29ddb0
Sergey Onufrienko <sergey@onufrienko.com>
2019-09-22 11:19:03 +0300

    Update debug messages

--- services/users.js
+++ services/users.js
@@ -7,47 +7,57 @@ const mongo = require('../db/mongo');
 // const user = await sequelize.Users.create(data);
 
 const getUsers = async () => {
-	const debug = Debug('getUsers');
-	debug('begin');
+	const debug = Debug('app:api:users');
+	debug('getUsers:start');
 
 	const dataFromMongo = await mongo.db
 		.collection('users')
 		.find({})
 		.sort()
 		.toArray();
-	debug('data from MongoDB');
+
+	debug('getUsers:end');
 
 	return dataFromMongo;
 };
 
 const getSingleUser = async userId => {
+	const debug = Debug('app:api:users');
+	debug('getSingleUser:start');
 	const user = await mongo.db
 		.collection('users')
 		.findOne({ _id: new ObjectID(userId) });
 
+	debug('getSingleUser:end');
 	return user;
 };
 
 const addUser = async data => {
-	const debug = Debug('addUser');
-	debug('begin');
+	const debug = Debug('app:api:users');
+	debug('add:start');
 
 	const insertResult = await mongo.db.collection('users').insertOne(data);
 	const userFromMongo = insertResult.ops[0];
-	debug('data from MongoDB');
+	debug('add:end');
 
 	return userFromMongo;
 };
 
 const updateUser = async (userId, data) => {
+	const debug = Debug('app:api:users');
+	debug('update:start');
 	await mongo.db
 		.collection('users')
 		.updateOne({ _id: new ObjectID(userId) }, { $set: data });
+	debug('update:end');
 };
 
 const deleteUser = async userId => {
 	try {
+		const debug = Debug('app:api:users');
+		debug('delete:start');
 		await mongo.db.collection('users').deleteOne({ _id: new ObjectID(userId) });
+		debug('delete:end');
 		return true;
 	} catch (e) {
 		return false;
